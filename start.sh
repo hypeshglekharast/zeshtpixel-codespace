8efec04eb7c81e149f405523674107c9a5eee1d1
hypeshglekharast <peshgelha@atomicmail.io>
2026-09-03 16:10:24 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-echo "🔧 Starting ZBProxy with custom config..."
+wget -O ZBProxy https://github.com/layou233/ZBProxy/releases/download/v3.1.0/ZBProxy-linux-amd64-v1
+chmod +x ZBProxy
+echo "✅ ZBProxy ready! Run ./ZBProxy to start."
 ./ZBProxy
